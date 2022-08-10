fe83474792360866b17b1786ed2d144bf0af329e
Matthew Fishman <mfishman@flatironinstitute.org>
2022-08-10 15:08:49 -0400

    First commit

--- src/ITensorCorrelators.jl
+++ src/ITensorCorrelators.jl
@@ -1,5 +1,20 @@
 module ITensorCorrelators
 
-# Write your package code here.
+using ITensors
+
+# correlator(("A", "B", "C", "D"), [(1, 2, 3, 4), (1, 2, 4, 5), ...])
+function correlator(
+  ops::NTuple{4,String},
+  sites::Vector{NTuple{4,Int}},
+  psi::MPS,
+)
+  s = siteinds(psi)
+  n = length(s)
+  sites = sort(sites)
+  # Check that each elements of `sites` is sorted
+
+end
+
+export correlator
 
 end
