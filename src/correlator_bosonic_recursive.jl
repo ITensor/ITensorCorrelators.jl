997f1576940666ff0c428c88e25912d38ee2529d
hankarls <hankarls@pks.mpg.de>
2024-06-04 11:07:08 +0200

    Optimized the branching of different set of indices (working this time)

--- src/correlator_bosonic_recursive.jl
+++ src/correlator_bosonic_recursive.jl
@@ -21,12 +21,13 @@ function correlator_recursive_compact(
 
   orthogonalize!(psi, 1)
   psi_dag = prime(linkinds, dag(psi))
-  #op_inds = unique(getindex.(sites, 1))
-  #op_inds = getindex.(sites, 1)
-  #repeats = [count(==(sites[idx][1]),sites[idx])-1 for idx=1:length(sites)]   # counts how many times site index is repeated in site. repeat=0 means index only occurs once.
-  #op_inds = unique([(sort([sites[idx]...])[1],count(==(sort([sites[idx]...])[1]),sites[idx])-1,sortperm([sites[idx]...])) for idx=1:length(sites)])
-  op_inds = unique([(sort([sites[idx]...])[1],count(==(sort([sites[idx]...])[1]),sites[idx])-1,sortperm([sites[idx]...])[1:count(==(sort([sites[idx]...])[1]),sites[idx])]) for idx=1:length(sites)])
-  #@show sites
+  
+  inds_ord = [sort([sites[idx]...])[1] for idx=1:length(sites)]
+  repeats = [count(==(sort([sites[idx]...])[1]),sites[idx])-1 for idx=1:length(sites)]
+  perms = [sortperm([sites[idx]...])[1:count(==(sort([sites[idx]...])[1]),sites[idx])] for idx=1:length(sites)]
+  
+  op_inds = unique([(inds_ord[idx],repeats[idx],perms[idx]) for idx=1:length(sites)])
+
   #@show op_inds
   s = siteinds(psi) #this can be done before orth.
   ln = linkinds(psi)
@@ -165,12 +166,16 @@ function add_operator_fermi(
 )
 
   for (a, op_ind) in enumerate(op_inds)
+
     #@show counter
-    #@show op_ind
+
     repeat = op_ind[2]  # counts how many times op_ind is repeated. repeat=0 means op_ind occurs once.
     perm_ind = op_ind[3]  # determines what operator acts on the site index
     op_ind = op_ind[1]
-    #element[counter] = op_ind
+
+    #@show repeat
+    #@show op_ind
+    #@show perm_ind
     
     element[counter:counter+repeat] .= op_ind
     if counter == 1
@@ -188,28 +193,8 @@ function add_operator_fermi(
       op_psi = apply(op("F", s[op_ind]), op_psi) #apply jordan wigner string if needed
     end
 
-    # fix this here, should not return tuples that are not part of the branch
-    #sites_ind = sites_ind_prev[findall(x -> x[counter] == op_ind, sites_ind_prev)]
-    #@show sites_ind_prev
-    #if counter+repeat == N
-    #  sites_ind = sites_ind_prev[findall(x -> x[counter] == op_ind, sites_ind_prev)]
-    #else
-    #  sites_ind = sites_ind_prev[findall(x -> x[counter] == op_ind && x[counter+1] != op_ind, sites_ind_prev)]
-    #end
-    #@show counter
-    #@show op_ind
-    #@show sites_ind
-    #perm = sortperm([sites_ind[1]...])
-    #@show perm
-    #@show element
-    #@show sites_ind[1]
-    #op_psi = apply(op(ops[counter], s[op_ind]), op_psi)  #apply operator in the spot #counter
-    # operator to the right acting first
-    #@show perm_ind
-    #@show ops
     for i=0:repeat
-      #op_psi = apply(op(ops[counter+repeat-i], s[op_ind]), op_psi)
-      #println("Applying operator ",ops[perm_ind[counter+repeat-i]], " on site ", op_ind)
+      #println("Applied ", ops[perm_ind[counter+repeat-i]], " on site ", op_ind)
       op_psi = apply(op(ops[perm_ind[counter+repeat-i]], s[op_ind]), op_psi)
     end
 
@@ -231,14 +216,8 @@ function add_operator_fermi(
 
     if counter+repeat == N
       R = ((op_ind) < length(psi) ? delta(dag(ln[op_ind]), ln[op_ind]') : ITensor(1.0)) #create right system
-      #C[tuple([element[k] for k in [findall(x -> x == j, indices)[1] for j in TupleTools.sort(indices)]]...)] = inner(
-      #  dag(L), R
-      #)
 
       fermions = [(ops[i] in ["C","Cdag","Cup","Cdagup","Cdn","Cdagdn"] ? 1 : 0) for i=1:length(ops)]
-      
-      #@show element
-      #@show perm_ind
 
       perm_elem = zeros(length(element))
       for i=1:length(element)
@@ -247,33 +226,46 @@ function add_operator_fermi(
 
       ferm_sites = Int64.(perm_elem[findall(x -> x in ["C","Cdag","Cup","Cdagup","Cdn","Cdagdn"],ops)])
       par = 1-2*parity(sortperm(ferm_sites))
-      #@show element[perm_ind]
-      #reorder element with permutations
-      #C[tuple(element[perm_ind]...)] = inner(dag(L),R)
-      #C[tuple((sort(element)[perm_ind])...)] = inner(dag(L),R)
+      
       C[tuple(perm_elem...)] = par*inner(dag(L),R)
-      #C[tuple(sites_ind[1]...)] = inner(dag(L),R)
-      #@show C[tuple(element...)]
-      #push!(C, tuple([element[k] for k in [findall(x->x==j,indices)[1] for j in sort(indices)]]...) => inner(dag(L), R))
       L = 0
     else
       #sites_ind = sites_ind_prev[findall(x -> x[counter+repeat] == op_ind, sites_ind_prev)] #checking if there are more terms with the element #counter in operators to compute
+      
+      
+      #sites_ind = sites_ind_prev[findall(x -> sort([x...])[counter+repeat] == op_ind, sites_ind_prev)]
       sites_ind = sites_ind_prev[findall(x -> sort([x...])[counter+repeat] == op_ind, sites_ind_prev)]
-      #@show sites_ind
-      #op_inds_next = unique(getindex.(sites_ind, counter + repeat + 1)) #getting the sites counter+1 in the string
-      #op_inds_next = getindex.(sites_ind, counter + repeat + 1) #getting the sites counter+repeat+1 in the string 
-      #repeats = [count(==(sites[idx][counter+repeat+1]),sites[idx])-1 for idx=1:length(sites)]   # counts how many times site index is repeated in site.
-      #op_inds_next = unique([(sites_ind[idx][counter+repeat+1],count(==(sites_ind[idx][counter+repeat+1]),sites_ind[idx])-1) for idx=1:length(sites_ind)])
-      #op_inds_next = unique([(sort([sites_ind[idx]...])[counter+repeat+1],count(==(sort([sites_ind[idx]...])[counter+repeat+1]),sites_ind[idx])-1,sortperm([sites_ind[idx]...])) for idx=1:length(sites_ind)])
-      op_inds_next = unique([(sort([sites_ind[idx]...])[counter+repeat+1],count(==(sort([sites_ind[idx]...])[counter+repeat+1]),sites_ind[idx])-1,sortperm([sites_ind[idx]...])[1:counter+repeat+count(==(sort([sites_ind[idx]...])[counter+repeat+1]),sites_ind[idx])]) for idx=1:length(sites_ind)])
-      #@show op_inds_next
-      #@show perm_ind
-      op_inds_next = op_inds_next[findall(x->x[3][1:counter+repeat]==perm_ind,op_inds_next)]
-      deleteat!(op_inds_next,findall(x->x[1]==op_ind,op_inds_next))
+      #sites_ind = sites_ind_prev[findall(x -> [x...][perm_ind[counter+repeat]] == op_ind, sites_ind_prev)]
+      repeat_next = [count(==(sort([sites_ind[idx]...])[counter+repeat+1]),sites_ind[idx])-1 for idx=1:length(sites_ind)]
+      inds_ord = [sort([sites_ind[idx]...])[counter+repeat+1] for idx=1:length(sites_ind)]
+      
+      sites_ind = sites_ind[findall(x -> x+counter+repeat<N,repeat_next)]
+      inds_ord = inds_ord[findall(x -> x+counter+repeat<N,repeat_next)]
+      repeat_next = repeat_next[findall(x -> x+counter+repeat<N,repeat_next)]
+
+      
+
       #@show counter
       #@show op_ind
+      #@show repeat
       #@show sites_ind
+      #@show inds_ord
+      #@show repeat_next
+      #perms = [sortperm([sites_ind[idx]...])[1:counter+repeat+count(==(sort([sites_ind[idx]...])[counter+repeat+1]),sites_ind[idx])] for idx=1:length(sites_ind)]
+      
+      #perms = [sortperm([sites_ind[idx]...])[1:counter+repeat+repeat_next[idx]+1] for idx=1:length(sites_ind)]
+      perms = [sortperm([sites_ind[idx]...])[1:counter+repeat+repeat_next[idx]+1] for idx=1:length(sites_ind)]
+
+      #@show perms
+
+      op_inds_next = unique([(inds_ord[idx],repeat_next[idx],perms[idx]) for idx=1:length(sites_ind)])
+
       #@show op_inds_next
+
+      op_inds_next = op_inds_next[findall(x->x[3][1:counter+repeat]==perm_ind,op_inds_next)]
+      
+      deleteat!(op_inds_next,findall(x->x[1]==op_ind,op_inds_next))
+
       for str in (op_ind + 1):(op_inds_next[1][1] - 1) #contract until the next operator to apply (with jw if required)
         if jw_next % 2 != 0
           L = L * apply(op("F", s[str]), psi[str]) * psi_dag[str]
