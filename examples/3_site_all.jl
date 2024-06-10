a3eedded84d73001a90d596d72ae54920bff3324
hankarls <hankarls@pks.mpg.de>
2024-06-11 00:37:52 +0200

    Added tests for 3- and 4-point correlators

---
+++ examples/3_site_all.jl
@@ -0,0 +1,45 @@
+using ITensors, ITensorMPS
+using IterTools
+using ProgressBars
+using ITensorCorrelators
+#include("../src/ITensorCorrelators.jl")
+
+
+function main(N)
+    operators = Dict([
+        ("Qubit", ["X","Y","iY","Z","H","√NOT","Phase","π/8","Proj0","Proj1"]), # same as S=1/2
+        ("S=1", ["Sz","Sz2","S+","S-","Sx","Sx2","iSy","Sy","Sy2"]),
+        ("Boson", ["A","Adag","N"]),    # same as qudit
+        ("Fermion", ["N","C","Cdag","F"]),
+        ("Electron", ["Ntot","Nup","Ndn","Cup","Cdn","Cdagup","Cdagdn","Sz","Sx","S+","S-","F","Fup","Fdn","Aup","Adn","Adagup","Adagdn"]),
+        ("tJ", ["Ntot","Nup","Ndn","Cup","Cdn","Cdagup","Cdagdn","Sz","Sx","S+","S-","F","Fup","Fdn","Aup","Adn","Adagup","Adagdn"])
+        ])
+    for type in keys(operators) 
+        println(type)
+        sites = siteinds(type,N)
+        psi = randomMPS(sites,20) + im*randomMPS(sites,20)
+        
+        # get all possible sets of indices
+        indices = vec([tuple(i,j,k) for i=1:N,j=1:N,k=1:N])
+
+        # construct all possible four point correlators
+        ops = [operators[type] for i=1:3]
+        for op in ProgressBar(IterTools.product(ops...))
+            # check parity of fermionic operators
+            ferms = [a in ["C","Cdag","Cup","Cdn","Cdagup","Cdagdn"] ? 1 : 0 for a in op]
+            if sum(ferms) // 2 != 0
+                continue
+            else
+                # using correlators
+                C = correlator(psi,tuple(op...),indices)
+                # computing old-fashioned way
+                for idx in indices
+                    ampo = OpSum()
+                    ampo += op[1],idx[1],op[2],idx[2],op[3],idx[3]
+                    mpo = MPO(ampo,sites)
+                    @assert isapprox(inner(psi',mpo,psi),C[idx],rtol=1e-12,atol=1e-12)
+                end
+            end
+        end
+    end
+end
---
+++ examples/4_point_correlator_full.jl
@@ -0,0 +1,20 @@
+using ITensors, ITensorMPS
+using ITensorCorrelators
+
+function main(N)
+    sites = siteinds("Electron",N)
+    psi = randomMPS(sites,20) + im*randomMPS(sites,20)
+        
+    # get all possible sets of indices
+    indices = vec([tuple(i,j,k,l) for i=1:N,j=1:N,k=1:N,l=1:N])
+    # using correlators
+    C = correlator(psi,("Cdagup","Cdagdn","Cdn","Cup"),indices)
+    # computing old-fashioned way
+    for idx in indices
+        ampo = OpSum()
+        ampo += "Cdagup",idx[1],"Cdagdn",idx[2],"Cdn",idx[3],"Cup",idx[4]
+        mpo = MPO(ampo,sites)
+        # checking that correlators are the same
+        @assert isapprox(inner(psi',mpo,psi),C[idx],rtol=1e-12,atol=1e-12)
+    end
+end
