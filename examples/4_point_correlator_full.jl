e20240e93fad755df5634eedf26be1deb4874db2
hankarls <hankarls@pks.mpg.de>
2024-06-14 22:13:30 +0200

    Added tests

--- examples/4_point_correlator_full.jl
+++ examples/4_point_correlator_full.jl
@@ -1,20 +1,31 @@
 using ITensors, ITensorMPS
 using ITensorCorrelators
 
+function correlator_mpo(psi, ops, op_sites)
+  s = siteinds(psi)
+  C = Dict{NTuple{4,Int},ComplexF64}()
+  for l in op_sites
+    os = OpSum()
+    os += ops[1], l[1], ops[2], l[2], ops[3], l[3], ops[4], l[4]
+    corr = MPO(os, s)
+    C[l...] = inner(psi', corr, psi)
+  end
+  return C
+end
+
 function main(N)
   sites = siteinds("Electron", N)
-  psi = random_mps(sites, 20) + im * random_mps(sites, 20)
+  ln = rand(1:50,length(sites)-1)
+  psi = randomMPS(sites, linkinds=ln) + im * randomMPS(sites, linkinds=ln)
 
   # get all possible sets of indices
   indices = vec([tuple(i, j, k, l) for i in 1:N, j in 1:N, k in 1:N, l in 1:N])
+
+  ops = ("Cdagup", "Cdagdn", "Cdn", "Cup")
+
   # using correlators
-  C = correlator(psi, ("Cdagup", "Cdagdn", "Cdn", "Cup"), indices)
-  # computing old-fashioned way
-  for idx in indices
-    o = OpSum()
-    o += "Cdagup", idx[1], "Cdagdn", idx[2], "Cdn", idx[3], "Cup", idx[4]
-    mpo = MPO(o, sites)
-    # checking that correlators are the same
-    @assert isapprox(inner(psi', mpo, psi), C[idx], rtol=1e-12, atol=1e-12)
-  end
+  correlators = @time correlator(psi, ops, indices)
+  correlators_mpo = @time correlator_mpo(psi, ops, indices)
+
+  return (; correlators, correlators_mpo)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -11,3 +11,40 @@ using Test
   vals_mpo = [correlators_mpo[op_site] for op_site in op_sites]
   @test vals ≈ vals_mpo rtol = 1e-12
 end
+
+@testset "Full 4 point correlator" begin
+  include(joinpath(pkgdir(ITensorCorrelators), "examples", "4_point_correlator_full.jl"))
+  res = main(; N=10)
+  correlators = res.correlators
+  correlators_mpo = res.correlators_mpo
+  op_sites = keys(correlators)
+  vals = [correlators[op_site] for op_site in op_sites]
+  vals_mpo = [correlators_mpo[op_site] for op_site in op_sites]
+  @test vals ≈ vals_mpo rtol = 1e-12
+end
+
+@testset "Mixed A and C operators" begin
+  Random.seed!(0)
+
+  sites = siteinds("Electron", 4)
+  psi = random_mps(sites)
+
+  op = ("Adagdn", "Adagup", "Cup", "Cdn")
+
+  # cannot handle correlators with both fermionic and non-fermionic operators,
+  # in particular, the sign is wrong when both an A- and C-operator act on the same site
+  inds = [(1, 3, 2, 1), (1, 2, 2, 3), (1, 2, 1, 3)]
+  C = correlator(psi, op, inds)
+  C2 = Dict{NTuple{4,Int},ComplexF64}()
+  for idx in inds
+    o = OpSum()
+    o += op[1], idx[1], op[2], idx[2], op[3], idx[3], op[4], idx[4]
+    mpo = MPO(o, sites)
+    C2[idx] = inner(psi', mpo, psi)
+  end
+
+  c = [C[idx] for idx in keys(C)]
+  c_mpo = [C2[idx] for idx in keys(C)]
+
+  @test_broken c ≈ c_mpo atol = 1e-12 rtol = 1e-12
+end
